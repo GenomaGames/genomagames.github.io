1f62295a714e099759469bce8f4a56274f9d5913
Alberto Fernandez <albertofdzm@gmail.com>
2021-12-07 00:21:05 +0100

    🔧 update gatsby-config to use ts-node
    Signed-off-by: Alberto Fernandez <albertofdzm@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,38 +1,4 @@
-module.exports = {
-  siteMetadata: {
-    siteUrl: "https://test-gatsby.genomagames.com",
-    title: "Genoma Games",
-  },
-  plugins: [
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "blog",
-        path: `${__dirname}/src/blog`,
-      },
-    },
-    "gatsby-plugin-react-helmet",
-    {
-      resolve: "gatsby-plugin-robots-txt",
-      options: {
-        policy: [
-          {
-            userAgent: "*",
-            disallow: ["/"],
-          },
-        ],
-      },
-    },
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    "gatsby-remark-images",
-    {
-      resolve: "gatsby-plugin-mdx",
-      options: {
-        extensions: [".mdx", ".md"],
-        gatsbyRemarkPlugins: ["gatsby-remark-images"],
-      },
-    },
-    "gatsby-transformer-sharp",
-  ],
-};
+require("source-map-support").install();
+require("ts-node").register();
+
+module.exports = require("./gatsby-config.ts");
---
+++ gatsby-config.ts
@@ -0,0 +1,57 @@
+import { GatsbyConfig } from "gatsby";
+
+const gatsbyConfig: GatsbyConfig = {
+  siteMetadata: {
+    siteUrl: "https://test-gatsby.genomagames.com",
+    title: "Genoma Games",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "data",
+        path: `${__dirname}/src/data/`,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "blog",
+        path: `${__dirname}/src/blog`,
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        policy: [
+          {
+            userAgent: "*",
+            disallow: ["/"],
+          },
+        ],
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-remark-images",
+    {
+      resolve: "gatsby-plugin-mdx",
+      options: {
+        extensions: [".mdx", ".md"],
+        gatsbyRemarkPlugins: ["gatsby-remark-images"],
+      },
+    },
+    "gatsby-transformer-json",
+  ],
+};
+
+export default gatsbyConfig;
