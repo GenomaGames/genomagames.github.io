4bf94f6089eff2918ea1cda5142302ac38dfcae8
Alberto Fernandez <albertofdzm@gmail.com>
2021-12-13 00:35:13 +0100

    🔧 update gatsby-node to create Post nodes from Mdx nodes
    Signed-off-by: Alberto Fernandez <albertofdzm@gmail.com>

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,6 +1,9 @@
 import fs from "fs";
+import path from "path";
 
 import { GatsbyGraphQLObjectType, GatsbyNode } from "gatsby";
+
+import { isObject } from "./src/utils/isObject";
 
 const gatsbyNode: GatsbyNode = {
   createSchemaCustomization: ({ actions, schema }) => {
@@ -37,6 +40,45 @@ const gatsbyNode: GatsbyNode = {
     });
 
     createTypes(mdxFrontmatterType);
+  },
+
+  onCreateNode: ({ actions, node, createNodeId, createContentDigest }) => {
+    if (node.internal.type === "Mdx") {
+      const frontmatterDate: string | null =
+        (isObject(node.frontmatter) &&
+          "date" in node.frontmatter &&
+          typeof node.frontmatter.date === "string" &&
+          node.frontmatter.date) ||
+        null;
+
+      let date: Date | null =
+        frontmatterDate !== null ? new Date(frontmatterDate) : null;
+
+      if (date === null && typeof node.fileAbsolutePath === "string") {
+        const basename: string = path.basename(node.fileAbsolutePath);
+
+        const basenameDate: string = basename.replace(
+          /(\d{4}-\d{2}-\d{2})?.*/,
+          "$1"
+        );
+
+        date = basenameDate ? new Date(basenameDate) : null;
+      }
+
+      const postNodeData = {
+        date: date,
+        id: createNodeId(node.id),
+        parent: node.id,
+      };
+
+      actions.createNode({
+        ...postNodeData,
+        internal: {
+          type: "Post",
+          contentDigest: createContentDigest(JSON.stringify(postNodeData)),
+        },
+      });
+    }
   },
 };
 
