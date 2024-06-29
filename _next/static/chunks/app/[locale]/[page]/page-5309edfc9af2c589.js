21f8ddd39930b994118739bb4ab854746e6f7bce
AlbertoFdzM <AlbertoFdzM@users.noreply.github.com>
2024-06-29 17:40:37 +0000

    deploy: 2888d1997386e696be7552c46532f637b753784c

--- _next/static/chunks/app/[locale]/[page]/page-5309edfc9af2c589.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[620,61,316],{2246:function(e,n,s){Promise.resolve().then(s.t.bind(s,413,23)),Promise.resolve().then(s.t.bind(s,8326,23))}},function(e){e.O(0,[413,326,971,472,744],function(){return e(e.s=2246)}),_N_E=e.O()}]);
---
+++ _next/static/chunks/app/[locale]/games/page-0329c0b0f6f67ad0.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[316,61,620],{3401:function(e,n,s){Promise.resolve().then(s.t.bind(s,8173,23)),Promise.resolve().then(s.t.bind(s,231,23))}},function(e){e.O(0,[173,231,971,23,744],function(){return e(e.s=3401)}),_N_E=e.O()}]);
